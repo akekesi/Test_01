60d987131a565d7190a170b38bfd9888fe9b8b77
attila kekesi <kekesi.att@gmail.com>
2022-01-29 17:37:18 +0100

    added func_create_directories()

--- test_create_directory.py
+++ test_create_directory.py
@@ -1,6 +1,27 @@
 import os
 
 
+def func_create_directories(path, structure):
+    """
+    Create directory structure
+    Args:
+        path:   path of main directory
+        structure:  structure of directories
+    Returns:
+        structure:  structure competed with paths 
+    """
+    for key in structure:
+        p = f"{path}/{key}"
+        if isinstance(structure[key], dict):
+            func_create_directory(p)
+            func_create_directories(p, structure[key])
+        else:
+            func_create_directory(p)
+            structure[key] = p
+    
+    return structure
+
+
 def func_create_directory(path):
     """
     Create directory
@@ -24,8 +45,29 @@ if __name__ == "__main__":
         r"C:\Users\kekes\OneDrive\Desktop\delete\00_test",
         r"C:\Users\kekes\OneDrive\Desktop\d\00_test"
     ]
+    structure = {
+        "00_a": {
+            "10_a": "",
+            "11_b": "",
+            "12_c": {
+                "20_a": "",
+                "21_b": ""
+            }
+        },
+        "01_a": {
+            "10_a": "",
+            "11_b": "",
+            "12_c": {
+                "20_a": "",
+                "21_b": ""
+            }
+        }
+    }
 
     # test
     for p in path:
         res = func_create_directory(p)
         print(f"func_create_directory --> {res}")
+    
+    res = func_create_directories(path[0], structure)
+    print(res)
