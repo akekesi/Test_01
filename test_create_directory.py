5ad44366afacdc125781167b9052e594bca062f1
attila kekesi <kekesi.att@gmail.com>
2022-01-29 16:40:26 +0100

    created test_created_directory.py

---
+++ test_create_directory.py
@@ -0,0 +1,28 @@
+import os
+
+from test_json import func_write_json
+
+
+def func_create_directory(path):
+    """
+    Create directory
+    Args:
+        path:   path of directory
+    Returns:
+        None
+    """
+    try:
+        os.mkdir(path)
+    except OSError as e:
+        print(f"--> not created {path}\n    {e}")
+    else:
+        print(f"--> created {path}")
+
+
+if __name__ == "__main__":
+    # value
+    path = r"C:\Users\kekes\OneDrive\Desktop\delete\00_test"
+
+    # test
+    res = func_create_directory(path)
+    print(f"func_create_directory --> {res}")
