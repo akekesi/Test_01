6b6a0cc4ca2a3f2bb972a6cbd871879c4556c536
attila kekesi <kekesi.att@gmail.com>
2022-01-29 12:06:22 +0100

    added new test case

--- test_json_unittest.py
+++ test_json_unittest.py
@@ -1,7 +1,9 @@
 import unittest
 from test_json import func_write_json
 
+
 class Test(unittest.TestCase):
+
 
     def test_func_wite_json(self):
         # values
@@ -10,8 +12,17 @@ class Test(unittest.TestCase):
             "b": 2,
             "c": 3
         }
-        path_1 = r"C:\Users\kekes\OneDrive\Desktop\delete.json"
+        data_2 = [
+            "a",
+            "b",
+            "c"
+        ]
+        path_1 = r"C:\Users\kekes\OneDrive\Desktop\delete\delete_1.json"
+        path_2 = r"C:\Users\kekes\OneDrive\Desktop\delete\delete_2.json"
+
+        # test
         self.assertEqual(func_write_json(path_1, data_1), None)
+        self.assertEqual(func_write_json(path_2, data_2), None)
 
 
 if __name__ == "__main__":
