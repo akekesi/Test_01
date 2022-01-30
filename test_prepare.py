85ccc774124d58d0e801bf5083aa211b00e3892c
attila kekesi <kekesi.att@gmail.com>
2022-01-30 20:02:21 +0100

    created test_prepare.py

---
+++ test_prepare.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+from test_create_directory import func_create_directory, func_create_directories
+
+path = r"C:\Users\kekes\OneDrive\Desktop\delete\00_prepare"
+structure = {
+    "00_json": "",
+    "00_txt": ""
+}
+files = {
+    "json": {
+        "json_00": "",
+        "json_01": "",
+        "json_02": ""
+    },
+    "txt": {
+        "txt_00": "",
+        "txt_01": "",
+        "txt_02": ""
+    }
+}
+
+shutil.rmtree(path)
+func_create_directory(path)
+structure_path = func_create_directories(path, structure)
+
+for key, value in files.items():
+    for k, v in value.items():
+        tmp_key = f"00_{key}"
+        path_f = f"{structure[tmp_key]}\{k}.{key}"
+        files[key][k] = path_f
+        if not os.path.exists(path_f):
+            with open(path_f, 'w'): pass
+print(files)
