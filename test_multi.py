9e838907f8e3519f3c408a364774f5e32a3c4201
attila kekesi <kekesi.att@gmail.com>
2022-02-01 21:47:12 +0100

    added multiprocessing

--- test_multi.py
+++ test_multi.py
@@ -1,5 +1,7 @@
 import time
 from threading import Thread
+from multiprocessing import Pool
+# idea: https://www.youtube.com/watch?v=7Nh5DhWKLoQ
 
 def func_wait(n: int, run_type: str) -> None:
     """
@@ -15,24 +17,38 @@ def func_wait(n: int, run_type: str) -> None:
     time.sleep(n)
     print(f"--> end {run_type}")
 
+
 if __name__ == "__main__":
-    n = 5
+    n_wait = 5
+    n_split = 5
 
     # singel
+    print(f"\nSINGLE START")
     start = time.time()
-    func_wait(n, "single")
+    func_wait(n_wait, "single")
     end = time.time()
-    print(f"--> single: {round(end - start, 2)}")
+    print(f"SINGLE END ({round(end - start, 2)}s)")
 
-    # thread
+    # threading
+    print(f"\nTHREADING START")
     threads = []
-    for i in range(n):
-        threads.append(Thread(target=func_wait, args=(n/5, f"thread-{i}")))
+    for i in range(n_wait):
+        threads.append(Thread(target=func_wait, args=(n_wait/n_split, f"threading-{i}")))
     start = time.time()
     for thread in threads:
         thread.start()
     for thread in threads:
         thread.join()
     end = time.time()
-    print(f"--> thread: {round(end - start, 2)}")
+    print(f"THREADING END ({round(end - start, 2)}s)")
 
+    # multiprocessing
+    print(f"\nMUTLIPROCESSING START")
+    start = time.time()
+    pool = Pool(processes=n_wait)
+    for i in range(n_wait):
+        pool.apply_async(func=func_wait, args=(n_wait/n_split, f"multiprocessing-{i}"))
+    pool.close()
+    pool.join()
+    end = time.time()
+    print(f"MULTIPROCESSING END ({round(end - start, 2)}s)")
